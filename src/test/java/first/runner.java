6ce1c853bc5dd0d960ccf4bba375c282d11f3ab4
Training_C2a.04.28 <Training_C2a.04.28@CDC2-D-4473N62.dir.svc.accenture.com>
2019-09-04 15:22:11 +0530

    dd

--- src/test/java/first/runner.java
+++ src/test/java/first/runner.java
@@ -6,7 +6,7 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(monochrome=true,plugin= {"html:reprt"})
+@CucumberOptions(monochrome=true,plugin= {"html:reprt"});
 public class runner {
 
 }
